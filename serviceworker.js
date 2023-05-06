91d84eefa05dd14ff7e78b4ff7d1cd95b8b22837
BeJuanitox <jsebastian23@hotmail.com>
2023-05-06 16:25:12 -0500

    Fetch

--- serviceworker.js
+++ serviceworker.js
@@ -1,1 +1,16 @@
-console.log('Hello from the sevice worker')
+const assets = ["/", "styles.css", "app.js", "sw-register.js",
+  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+
+self.addEventListener("install", event => {
+  caches.open("assets").then( cache => {
+    cache.addAll(assets);
+  });
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.url == "http://localhost:5500/fake") {
+    const response = new Response(`Hello, I'm a response on URL ${event.request.url}`);
+    event.respondWith(response);
+  }
+});
