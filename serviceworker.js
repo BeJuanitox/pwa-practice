57710e1d07caee68352fffa84ba6cb5360159a45
BeJuanitox <jsebastian23@hotmail.com>
2023-05-06 16:44:02 -0500

    Improve validations

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,32 @@
 const assets = ["/", "styles.css", "app.js", "sw-register.js",
-  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
-];
+  "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2" ];
 
 self.addEventListener("install", event => {
-  caches.open("assets").then( cache => {
-    cache.addAll(assets);
-  });
+  event.waitUntil(
+    caches.open("assets").then( cache => {
+      cache.addAll(assets);
+    })
+  )
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.url == "http://localhost:5500/fake") {
     const response = new Response(`Hello, I'm a response on URL ${event.request.url}`);
     event.respondWith(response);
+  } else {
+    // We want to try and see if the request is cached
+    event.respondWith(
+      caches.open("assets").then(cache => {
+        cache.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+            // It's a cache HIT
+            return cachedResponse;
+          } else {
+            // It's a cache MISS
+            return fetch(event.request);
+          }
+        })
+      })
+    )
   }
 });
